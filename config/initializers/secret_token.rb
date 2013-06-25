00a5744b7c44fab6baea2fa1ccd40b901bc6babc
Ryan Atallah <mail@ryanatallah.com>
2013-06-25 16:21:58 -0600

    Rails project initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BudgetRailsAngular::Application.config.secret_key_base = '9a688c153399c28da0cacde34ad62e4e92c2b93df2954bee093d02da0f6695c7a6c753a5b7a0237af3736452adf503bf9e099b0c8e3077a8ad4db869f5779a80'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BudgetRailsAngular::Application.config.session_store :cookie_store, key: '_budget-rails-angular_session'
